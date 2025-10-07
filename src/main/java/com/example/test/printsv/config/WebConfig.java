bbf3eb2171bda19c0dc71281d7a60d32b8af8a92
breakvirus <alexmazadon@gmail.com>
2025-10-07 08:28:11 -0500

    register and login init for react frontend with front

---
+++ src/main/java/com/example/test/printsv/config/WebConfig.java
@@ -0,0 +1,15 @@
+package com.example.test.printsv.config;
+
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class WebConfig implements WebMvcConfigurer {
+    public  void addCorsMapping(CorsRegistry registry){
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+
+}
