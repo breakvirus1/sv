95d67de5fac2e2eb9e3b8d8b340bcb1950ccf9c6
breakvirus <alexmazadon@gmail.com>
2025-10-09 00:45:05 -0500

    back and front in one repo

--- src/main/java/com/example/test/printsv/controller/HelloController.java
+++ src/main/java/com/example/test/printsv/controller/HelloController.java
@@ -1,5 +1,6 @@
 package com.example.test.printsv.controller;
 
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping("/hello")
     public String hello() {
-        return "Hello from the secured backend!";
+        String name =  SecurityContextHolder.getContext().getAuthentication().getName();
+        return "Hello from the secured backend, "+name;
     }
 }
