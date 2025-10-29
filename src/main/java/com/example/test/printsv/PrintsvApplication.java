f4f9340493f5976b09460851f559f47a2b9747e6
breakvirus <alexmazadon@gmail.com>
2025-10-29 03:29:14 -0500

    переделана инциализация ролей

--- src/main/java/com/example/test/printsv/PrintsvApplication.java
+++ src/main/java/com/example/test/printsv/PrintsvApplication.java
@@ -9,6 +9,8 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.DependsOn;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.example.test.printsv.entity.ERole;
 import com.example.test.printsv.entity.Role;
@@ -22,36 +24,48 @@ public class PrintsvApplication {
 	public static void main(String[] args) throws Exception {
 		SpringApplication.run(PrintsvApplication.class, args);
 	}
-//	@Bean
-//	CommandLineRunner cleanDatabase(JdbcTemplate jdbcTemplate) {
-//		return args -> {
-//			jdbcTemplate.execute("ALTER TABLE user_roles DROP CONSTRAINT IF EXISTS FKh8ciramu9cc9q3qcqiv4ue8a6");
-//			jdbcTemplate.execute("DELETE FROM user_roles");
-//			jdbcTemplate.execute("DELETE FROM users");
-//			jdbcTemplate.execute("DELETE FROM roles");
-//		};
-//	}
 
-	@Bean
-	CommandLineRunner initRoles(RoleRepository roleRepository) {
-        System.out.println("initialize roles...");
-		return args -> {
+	// @Bean
+    // @Transactional
+	// CommandLineRunner initRoles(RoleRepository roleRepository) {
+    //     System.out.println("initialize roles...");
+	// 	return args -> {
+	// 		// if (!roleRepository.existsByName(ERole.ROLE_MANAGER)) {
+	// 		// 	roleRepository.save(new Role(null, ERole.ROLE_MANAGER));
+    //         //     System.out.println("ROLE_MANAGER saved");
+	// 		// }
+	// 		// if (!roleRepository.existsByName(ERole.ROLE_ADMIN)) {
+	// 		// 	roleRepository.save(new Role(null, ERole.ROLE_ADMIN));
+    //         //     System.out.println("ROLE_ADMIN saved");
+	// 		// }
+	// 		// if (!roleRepository.existsByName(ERole.ROLE_OPERATOR)) {
+	// 		// 	roleRepository.save(new Role(null, ERole.ROLE_OPERATOR));
+    //         //     System.out.println("ROLE_OPERATOR saved");
+	// 		// }
+    //         // System.out.println("Roles in database: " + roleRepository.findAll());
+	// 	};
+	// }
+
+@Bean
+
+    CommandLineRunner initUsers(AuthService authService, RoleRepository roleRepository) {
+        System.out.println("FIND ROLES IN REPOSITORY: " + roleRepository.findAll());
+        return args -> {
 			if (!roleRepository.existsByName(ERole.ROLE_MANAGER)) {
 				roleRepository.save(new Role(null, ERole.ROLE_MANAGER));
+                System.out.println("ROLE_MANAGER saved");
 			}
 			if (!roleRepository.existsByName(ERole.ROLE_ADMIN)) {
 				roleRepository.save(new Role(null, ERole.ROLE_ADMIN));
+                System.out.println("ROLE_ADMIN saved");
 			}
 			if (!roleRepository.existsByName(ERole.ROLE_OPERATOR)) {
 				roleRepository.save(new Role(null, ERole.ROLE_OPERATOR));
+                System.out.println("ROLE_OPERATOR saved");
 			}
-		};
-	}
+            System.out.println("Roles in database: " + roleRepository.findAll());
 
-@Bean
-    CommandLineRunner initUsers(AuthService authService, RoleRepository roleRepository) {
-        System.out.println("FIND ROLES IN REPOSITORY: "+roleRepository.findAll());
-        return args -> {
+
             Role adminRole = roleRepository.findByName(ERole.ROLE_ADMIN)
                     .orElseThrow(() -> new RuntimeException("Role ROLE_ADMIN not found"));
             Role managerRole = roleRepository.findByName(ERole.ROLE_MANAGER)
@@ -59,19 +73,16 @@ public class PrintsvApplication {
             Role operatorRole = roleRepository.findByName(ERole.ROLE_OPERATOR)
                     .orElseThrow(() -> new RuntimeException("Role ROLE_OPERATOR not found"));
 
-        
             RegisterRequest adminRequest = new RegisterRequest();
             adminRequest.setUsername("admin");
             adminRequest.setPassword("111111");
             Set<Role> adminRoles = new HashSet<>();
             adminRoles.add(adminRole);
-            System.out.println(Arrays.asList(adminRoles));
+            // System.out.println(Arrays.asList(adminRoles));
 
-            
-            authService.registerForInit(adminRequest,adminRoles);
+            authService.registerForInit(adminRequest, adminRoles);
             System.out.println("Created user: " + adminRequest.toString());
 
-            
             RegisterRequest managerRequest = new RegisterRequest();
             managerRequest.setUsername("manager");
             managerRequest.setPassword("111111");
@@ -80,14 +91,14 @@ public class PrintsvApplication {
             authService.registerForInit(managerRequest, managerRoles);
             System.out.println("Created user: " + managerRequest.toString());
 
-            
             RegisterRequest operatorRequest = new RegisterRequest();
             operatorRequest.setUsername("operator");
             operatorRequest.setPassword("111111");
             Set<Role> operatorRoles = new HashSet<>();
             operatorRoles.add(operatorRole);
-            authService.registerForInit(operatorRequest,operatorRoles);
+            authService.registerForInit(operatorRequest, operatorRoles);
             System.out.println("Created user: " + operatorRequest.toString());
         };
     }
 }
+
