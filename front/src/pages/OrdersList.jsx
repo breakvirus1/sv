9ae7781af31881e3edd6b11812778843fd1b1f4a
breakvirus <alexmazadon@gmail.com>
2026-09-21 21:08:37 +0300

    fix: set InfiniteScroll threshold to 300px for bottom-edge pagination trigger

--- front/src/pages/OrdersList.jsx
+++ front/src/pages/OrdersList.jsx
@@ -359,7 +359,7 @@ const OrdersList = () => {
             dataLength={sortedOrders.length}
             next={fetchNext}
             hasMore={!!hasNextPage}
-            threshold={0}
+            threshold={300}
             loader={
               <Box display="flex" justifyContent="center" alignItems="center" py={2}>
                 <CircularProgress size={24} />
